379015e4dfbff786abd800c3fbda498999e5d394
OmorFaruk00 <89313105+OmorFaruk00@users.noreply.github.com>
2022-09-30 15:07:33 +0600

    monthly

--- app/Http/Controllers/Api/MonthlyAdmissionStudentController.php
+++ app/Http/Controllers/Api/MonthlyAdmissionStudentController.php
@@ -47,7 +47,7 @@ class MonthlyAdmissionStudentController extends Controller
                 ->get()
                 ->groupBy(function($d) { return Carbon::parse($d->adm_date)->format('M');})->map(function ($students, $month) use($list){
                     return [                        
-                        $month => count($students),
+                        $month. ', '.Carbon::parse($list->adm_date)->format('Y') => count($students),
                     ];
                 })->toArray());
         }
